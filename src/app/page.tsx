35dc1ec52dc3084aaa3182b5d985c5c27c4330ca
KhanhMinh1392 <81310329+KhanhMinh1392@users.noreply.github.com>
2024-07-14 13:45:41 +0700

    feat: create intro ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Sidebar from '@/components/sidebar';
-import { ChevronDown, Grip, Sparkle } from 'lucide-react';
+import { VToolTip } from '@/components/tooltip';
+import { Input } from '@/components/ui/input';
+import { ChevronDown, Compass, Grip, ImagePlus, Lightbulb, Mic, Sparkle } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,7 +10,7 @@ export default function Home() {
     <main className="flex min-h-screen">
       <Sidebar />
       <section className="flex-1">
-        <header className="sticky top-0 mx-5 mt-4 flex items-center justify-between">
+        <nav className="sticky top-0 mx-5 my-4 flex items-center justify-between">
           <h1 className="flex items-center gap-2 text-xl">
             Gemini <ChevronDown width={14} height={14} />
           </h1>
@@ -32,7 +34,74 @@ export default function Home() {
               className="h-9 w-9 rounded-full object-cover"
             />
           </div>
-        </header>
+        </nav>
+        <article className="max-h-[calc(100vh-180px)] w-full overflow-auto px-5 pt-4">
+          <div className="mx-auto max-w-4xl">
+            <h1 className="mx-2 mt-6 inline-block bg-gradient-to-r from-blue-500 via-purple-500 to-orange-500 bg-clip-text text-[3.2rem] font-medium leading-tight text-transparent">
+              Xin chào Khánh Minh!
+            </h1>
+            <h1 className="mx-2 mb-8 text-[3.2rem] font-medium leading-tight text-gray-400">
+              Hôm nay tôi có thể giúp gì cho bạn?
+            </h1>
+            <div className="mb-3 mt-16 flex items-center justify-evenly gap-2">
+              <div className="flex h-[12.5rem] w-[12.5rem] flex-col justify-between rounded-xl bg-gray-100 p-4">
+                <p>Thói quen trong gia đình</p>
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                  <Lightbulb />
+                </div>
+              </div>
+              <div className="flex h-[12.5rem] w-[12.5rem] flex-col justify-between rounded-xl bg-gray-100 p-4">
+                <p>Ý tưởng nhân vật cho tiểu thuyết</p>
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                  <Compass />
+                </div>
+              </div>
+              <div className="flex h-[12.5rem] w-[12.5rem] flex-col justify-between rounded-xl bg-gray-100 p-4">
+                <p>Các loại chế độ ăn</p>
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                  <Lightbulb />
+                </div>
+              </div>
+              <div className="flex h-[12.5rem] w-[12.5rem] flex-col justify-between rounded-xl bg-gray-100 p-4">
+                <p>Ý tưởng đặt tên sản phẩm</p>
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                  <Compass />
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className="h-[calc(100svh-640px)]">
+          <div className="mx-auto mt-4 max-w-[940px]">
+            <div className="relative mx-auto w-[880px] px-4">
+              <Input
+                placeholder="Nhập câu lệnh tại đây"
+                className="h-16 rounded-full border-none bg-gray-100 pl-6 text-lg placeholder:text-base focus:bg-gray-200 focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0"
+              />
+              <div className="absolute right-8 top-2">
+                <div className="flex items-center">
+                  <VToolTip placeholder="Tải ảnh lên" direction="bottom" position="center">
+                    <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
+                      <ImagePlus width={20} height={20} />
+                    </div>
+                  </VToolTip>
+                  <VToolTip placeholder="Sử dụng micro" direction="bottom" position="center">
+                    <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
+                      <Mic width={20} height={20} />
+                    </div>
+                  </VToolTip>
+                </div>
+              </div>
+            </div>
+            <p className="mt-2 text-center text-xs">
+              Gemini có thể đưa ra thông tin không chính xác, kể cả thông tin về con người, vì vậy, hãy xác minh các câu
+              trả lời của Gemini. <br />{' '}
+              <a href="" className="underline">
+                Quyền riêng tư của bạn và Các ứng dụng Gemini
+              </a>
+            </p>
+          </div>
+        </article>
       </section>
     </main>
   );
